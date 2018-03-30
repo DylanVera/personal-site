1aa5c4c6ff58d7024263c296ae35c0996580d868
dylanvera <Dylan.Vera@student.csulb.edu>
2018-03-30 21:27:21 +0000

    typos

--- js/components/Nav.react.js
+++ js/components/Nav.react.js
@@ -5,13 +5,15 @@ var IndexLink = require('react-router').IndexLink;
 var Nav = React.createClass({
    render: function() {
         return(
-            <div className="nav-div">
-                <h1 className="title-banner"><IndexLink to='/'>Dylan Vera</IndexLink></h1>
-                <ul className="nav-links">
-                    <li><Link to="/projects">Projects</Link></li>
-                    <li><Link to="/resume">Resume</Link></li>
-                </ul>
-            </div>
+            <header>
+                <nav>
+                    <h1 className="title-banner"><IndexLink to='/'>Dylan Vera</IndexLink></h1>
+                    <ul className="nav-links">
+                        <li><Link to="/projects">Projects</Link></li>
+                        <li><Link to="/resume">Resume</Link></li>
+                    </ul>
+                </nav>
+            </header>
         )
    }
 });
